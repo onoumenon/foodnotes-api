9c163e64981e5ef7d2fae7ec9c20bedc500521de
Jesstern Rays <jsstrn@users.noreply.github.com>
2019-03-20 14:22:38 +0800

    Make author field required

--- models/book.js
+++ models/book.js
@@ -5,7 +5,10 @@ const bookSchema = new mongoose.Schema({
     type: String,
     required: true
   },
-  author: String
+  author: {
+    type: String,
+    required: true
+  }
 });
 
 const Book = mongoose.model("Book", bookSchema);

c675252704b3398219beb88d80fb3262a56879b7
huitian <tanhuitian@gmail.com>
2019-03-29 12:15:55 +0800

    Fetch geocoords before saving in POST and PUT

--- models/place.js
+++ models/place.js
@@ -1,6 +1,5 @@
 const mongoose = require("mongoose");
-// regex to filter out unit number from addresses for openCage api
-const unitNoRegex = /#\d+-*\d+/g;
+const opencage = require("opencage-api-client");
 
 const placeSchema = new mongoose.Schema(
   {
@@ -32,7 +31,76 @@ const placeSchema = new mongoose.Schema(
   { timestamps: false }
 );
 
-// placeSchema.pre("save", function(error, res, next) {});
+placeSchema.pre("findOneAndUpdate", function(next) {
+  // format addresses for opencage API
+  const unitNoRegex = /#\d+-*\d+/g;
+  const countryRegex = /Singapore/i;
+  let newAddress = this._update.address.replace(unitNoRegex, "");
+  if (newAddress.search(countryRegex) === -1) {
+    newAddress = newAddress + ", Singapore";
+  }
+
+  // TO DO (Pass in country in user settings)
+  opencage
+    .geocode({ q: newAddress })
+    .then(data => {
+      if (data.status.code == 200) {
+        if (
+          data.results.length > 0 &&
+          data.results[0].annotations.timezone.name === "Asia/Singapore"
+        ) {
+          const place = data.results[0];
+          console.log(place.geometry.lng, place.geometry.lat);
+          this._update.location = {
+            type: "Point",
+            coordinates: [place.geometry.lng, place.geometry.lat]
+          };
+        }
+      } else if (data.status.code == 402) {
+        throw new Error("Hit free trial limit");
+      } else {
+        throw new Error(data.status.message);
+      }
+      next();
+    })
+    .catch(error => {
+      console.log("error", error.message);
+      next(error);
+    });
+});
+
+placeSchema.pre("save", function(next) {
+  // format addresses for opencage API
+  const unitNoRegex = /#\d+-*\d+/g;
+  const countryRegex = /Singapore/i;
+  let newAddress = this.address.replace(unitNoRegex, "");
+  if (newAddress.search(countryRegex) === -1) {
+    newAddress = newAddress + ", Singapore";
+  }
+  // TO DO (Pass in country in user settings)
+  opencage
+    .geocode({ q: newAddress })
+    .then(data => {
+      if (data.status.code == 200) {
+        if (
+          data.results.length > 0 &&
+          data.results[0].annotations.timezone.name === "Asia/Singapore"
+        ) {
+          const place = data.results[0];
+          this.location.coordinates = [place.geometry.lng, place.geometry.lat];
+        }
+      } else if (data.status.code == 402) {
+        throw new Error("Hit free trial limit");
+      } else {
+        throw new Error(data.status.message);
+      }
+      next();
+    })
+    .catch(error => {
+      console.log("error", error.message);
+      next(error);
+    });
+});
 
 placeSchema.post("save", function(error, res, next) {
   if (error.name === "MongoError" && error.code === 11000) {
